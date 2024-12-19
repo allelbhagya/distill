6337f0338541f0b00b39bba00aac294282b8ad32
Bhagyalaxmi <80905783+allelbhagya@users.noreply.github.com>
2024-12-19 21:11:08 +0530

    distill

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import Foot from "./Foot";
 import NavBar from "./NavBar";
 
@@ -8,8 +9,57 @@ export default function About(){
                 <NavBar/>
             </div>
             <div>
-                About page
+                <div className="head-section">
+                Machine Learning Research<br></br>
+                Should Be Clear, Dynamic and Vivid.<br></br>
+               <b>Distill</b> is Here to Help.
+                </div>
             </div>
+            <div className="two-navs">
+  {[
+    {
+      to: "/journal",
+      heading: "A JOURNAL",
+      description: "Devoted to clear explanations, native to the Web.",
+    },
+    {
+      to: "/prize",
+      heading: "$10,000 PRIZES",
+      description: "For outstanding work communicating and refining ideas.",
+    },
+  ].map((item, index) => (
+    <Link to={item.to} key={index} className="two-nav-item">
+      <div>
+        <p>
+          {item.heading}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
+            />
+          </svg>
+        </p>
+        <p>{item.description}</p>
+      </div>
+    </Link>
+  ))}
+</div>
+
+<div className="content">
+<p className="text-check">A modern medium for presenting research</p>
+The web is a powerful medium to share new ways of thinking. Over the last few years we’ve seen many imaginative examples of such work. But traditional academic publishing remains focused on the PDF, which prevents this sort of communication.
+</div>
+
             <div>
                 <Foot/>
             </div>
--- src/components/Prize.tsx
+++ src/components/Prize.tsx
@@ -7,8 +7,9 @@ export default function Prize(){
             <div>
                 <NavBar/>
             </div>
-            <div>
-                Prize page
+            <div className="content">
+                <h2>The Distill Prize for Clarity in Machine Learning</h2>
+                <p>Recognizing outstanding work communicating and refining ideas in Machine Learning and adjacent topics.</p>
             </div>
             <div>
                 <Foot/>
